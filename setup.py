2a9352473de0bb0cc65d2fe3c45be4e79b826d8e
drmalex07 <alexakis@imis.athena-innovation.gr>
2014-05-01 23:09:16 +0300

    Creating basic PyPi package structure

--- setup.py
+++ setup.py
@@ -1,22 +1,22 @@
 from setuptools import setup, find_packages
 import sys, os
 
-version = '0.8'
+version = '0.1dev'
 
 setup(
-	name='ckanext-helloworld',
+	name='ckanext-publicamundi',
 	version=version,
-	description="hello world",
+	description="A collection of CKAN plugins developed for PublicaMundi project",
 	long_description="""\
 	""",
 	classifiers=[], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
 	keywords='',
-	author='malex',
-	author_email='malex@example.net',
+	author='Michail Alexakis',
+	author_email='alexakis@imis.athena-innovation.gr',
 	url='',
 	license='',
 	packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
-	namespace_packages=['ckanext', 'ckanext.helloworld'],
+	namespace_packages=['ckanext', 'ckanext.publicamundi'],
 	include_package_data=True,
 	zip_safe=False,
 	install_requires=[
@@ -27,11 +27,11 @@ setup(
 	"""
         [ckan.plugins]
 
-        helloworld_dataset = ckanext.helloworld.plugins:DatasetForm
+        publicamundi_dataset = ckanext.publicamundi.plugins:DatasetForm
 
         [paste.paster_command]
 
-        helloworld-greet = ckanext.helloworld.commands:Greet
+        publicamundi-greet = ckanext.publicamundi.commands:Greet
 
         [fanstatic.libraries]
 
