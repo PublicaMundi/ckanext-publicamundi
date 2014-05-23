6d168b1e41c06424ab5ceddaa92385ec16a81db7
smanousopoulos <smanousopoulos@gmail.com>
2014-05-23 19:28:30 +0300

    added INSPIRE schema data

--- initial-data/types.py
+++ initial-data/types.py
@@ -14,12 +14,43 @@ class PointOfContact(BaseObject):
     def __init__(self, **kwargs):
         BaseObject.__init__(self, **kwargs)
 
+class Identifier(BaseObject):
+    zope.interface.implements(IIdentifier)
+        
+    code = None
+    codespace = None
+    
+    def __init__(self, **kwargs):
+        BaseObject.__init__(self, **kwargs)
+
+class GeographicBoundingBox(BaseObject):
+    zope.interface.implements(IGeographicBoundingBox)
+    
+    northBoundLatitude = None
+    southhBoundLatitude = None
+    eastBoundLongitude = None
+    westBoundLongitude = None
+    
+    def __init__(self, **kwargs):
+        BaseObject.__init__(self, **kwargs)
+
 class InspireMetadata(BaseMetadata):
     zope.interface.implements(IInspireMetadata)
     
     metadataPointOfContact = list
     metadataDate = None
     metadataLanguage = None
+    
+    identificationResourceTitle = None
+    identificationIdentifier = List
+    identificationResourceAbstract = None
+    identificationResourceLocator = List
+    identificationResourceLanguage = List
 
+    classificationTopicCategory = None
+
+    geographicBoundingBox = GeographicBoundingBox
+    geographicCountries = None
+    
     def __init__(self, **kwargs):
         BaseMetadata.__init__(self, **kwargs)
