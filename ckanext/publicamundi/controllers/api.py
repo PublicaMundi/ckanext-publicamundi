414b7febef01cc562c26a4e898ec79acb7b0969f
drmalex07 <alexakis@imis.athena-innovation.gr>
2014-06-16 04:13:59 -0400

    Added IRoutes, IActions interfaces to basic plugin.

---
+++ ckanext/publicamundi/controllers/api.py
@@ -0,0 +1,44 @@
+import logging
+import json
+import time
+import datetime
+
+from pylons import g
+from pylons.i18n import _
+
+from   ckan.lib.base import (BaseController, c, request, response, render, abort, redirect)
+import ckan.model           as model
+import ckan.plugins         as p
+import ckan.plugins.toolkit as toolkit
+import ckan.logic           as logic
+
+import ckanext.publicamundi
+import ckanext.publicamundi.lib.actions as publicamundi_actions
+
+log1 = logging.getLogger(__name__)
+
+CONTENT_TYPES = {
+    'json': 'application/json; charset=utf8',
+}
+
+class Controller(BaseController):
+
+    def mimetype_autocomplete(self):
+        q = request.params.get('incomplete', '')
+        limit  = request.params.get('limit', 10)
+
+        context = { 'model': model, 'session': model.Session }
+        data_dict = { 'q': q, 'limit': limit }
+
+        r = logic.get_action('mimetype_autocomplete')(context, data_dict)
+
+        result_set = {
+            'ResultSet': {
+                'Result': [{'name': t } for t in r]
+            }
+        }
+
+        response.headers['Content-Type'] = CONTENT_TYPES['json']
+        return [json.dumps(result_set)]
+
+
