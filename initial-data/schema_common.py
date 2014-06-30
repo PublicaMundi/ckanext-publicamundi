51e23521b8dff24ed303362c7ae0d53bf7f90324
steliosman <smanousopoulos@imis.athena-innovation.gr>
2014-06-30 18:05:49 +0300

    removed max dates again

---
+++ initial-data/schema_common.py
@@ -0,0 +1,137 @@
+import zope.interface
+import zope.schema
+from zope.schema.vocabulary import SimpleVocabulary
+import re
+import datetime
+import vocabularies 
+import helper as h
+
+class IResponsibleParty(zope.interface.Interface):
+	organization = zope.schema.TextLine(
+		title = u'Organization name',
+		required = True,
+		min_length = 2)
+	email = zope.schema.List(
+		title = u'Email',
+		required = True,
+		min_length = 1,
+		value_type = zope.schema.TextLine(
+			title = u'Email',
+			constraint = re.compile("[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?").match))
+
+	role = zope.schema.Choice(h.get_terms('party_roles'),
+		title = u'Responsible party role',
+		description = u'This is the role of the responsible organisation.',
+		required = True)
+	
+class IFreeKeyword(zope.interface.Interface):
+	value = zope.schema.TextLine(
+		title = u"Keyword value",
+		description = u"The keyword value is a commonly used word, formalised word or phrase used to describe the subject. While the topic category is too coarse for detailed queries, keywords help narrowing a full text search and they allow for structured keyword search.\nThe value domain of this metadata element is free text.",
+		required = False)
+
+	originating_vocabulary = zope.schema.TextLine(
+		title = u'Title',
+		description = u"If the keyword value originates from a controlled vocabulary (thesaurus, ontology), for example GEMET, the citation of the originating controlled vocabulary shall be provided.\nThis citation shall include at least the title and a reference date (date of publication, date of last revision or of creation) of the originating controlled vocabulary.",
+		required = False)
+	reference_date = zope.schema.Date(
+		title = u'Reference date',
+		required = False,
+        )
+	date_type = zope.schema.Choice(h.get_terms('date_types'),
+		title = u'Date Type',
+		required = False)
+	
+	@zope.interface.invariant
+    	def check_case_mandatory(obj):
+		_mandatory_list.append(obj)
+		
+		if obj.value or obj.originating_vocabulary or obj.reference_date or obj.date_type:
+			if not obj.value or not obj.originating_vocabulary or not obj.reference_date or not obj.date_type:
+				raise zope.interface.Invalid('You need to fill in the rest Free Keyword fields')
+
+class IGeographicBoundingBox(zope.interface.Interface):
+	nblat = zope.schema.Float(
+		title = u'North Bound Latitude',
+		min = -90.0,
+		max = 90.0,
+		required = True)
+	sblat = zope.schema.Float(
+		title = u'South Bound Latitude',
+		min = -90.0,
+		max = 90.0,
+		required = True)
+	eblng = zope.schema.Float(
+		title = u'East Bound Longitude',
+ 		min = -180.0,
+		max = 180.0,
+		required = True)
+	wblng = zope.schema.Float(
+		title = u'West Bound Longitude',
+		min = -180.0,
+		max = 180.0,
+		required = True)
+
+_a_greater_b_called = []
+
+class ITemporalExtent(zope.interface.Interface):
+	start = zope.schema.Date(
+		title = u'Starting date',
+		required = False,
+        )
+
+	end = zope.schema.Date(
+		title = u'Ending date',
+		required = False,
+        )
+
+	@zope.interface.invariant
+    	def check_date_order(obj):
+		_a_greater_b_called.append(obj)
+		
+		if obj.start > obj.end:
+			raise zope.interface.Invalid('Start date (%s) later than end date (%s)' % (obj.start,obj.end))
+
+
+_mandatory_list = []
+
+class ISpatialResolution(zope.interface.Interface):
+		
+	distance = zope.schema.Int(
+		title = u'Resolution distance',
+		required = False)
+
+	uom = zope.schema.TextLine(
+		title = u'Unit of measure',
+		required = False,
+		min_length = 2)
+
+	@zope.interface.invariant
+    	def check_case_mandatory(obj):
+		_mandatory_list.append(obj)
+		
+		if obj.denominator or obj.distance or obj.uom:
+			if not obj.denominator or not obj.distance or not obj.uom:
+				raise zope.interface.Invalid('You need to fill in the rest Spatial Resolution fields')
+
+class IConformity(zope.interface.Interface):
+
+	title = zope.schema.TextLine(
+		title = u'Specifications',
+		required = True)
+
+	date = zope.schema.Date(
+		title = u'Date',
+		required = True,
+        )
+
+	date_type = zope.schema.Choice(h.get_terms('date_types'),
+		title = u'Date type',
+		required = True)
+
+	degree = zope.schema.Choice(h.get_terms('degrees'),
+		title = u'Degree',
+		description = u'This is the degree of conformity of the resource to the implementing rules adopted under Article 7(1) of Directive 2007/2/EC or other specification.',
+		default = "not_evaluated",
+		required = True)
+
---
+++ initial-data/test_inspire.py
@@ -0,0 +1,139 @@
+import zope.interface
+import zope.schema
+import datetime
+from typedecl_common import *
+from typedecl import *
+
+print 'Testing Metadata\n'
+# if date entered wrong datetime.datetime raises error
+
+###	def __init__(self,contact,datestamp,languagecode,title,identifier,abstract,locator,resource_language,topic_category,keywords, bounding_box,temporal_extent,creation_date,publication_date,revision_date,lineage,spatial_resolution,conformity,access_constraints,limitations,responsible_party):
+
+
+print 'Test Case #1'
+print 'all correct\n'
+
+psoc = InspireMetadata(
+[ResponsibleParty(u"Org",[u"email@asd.gr"],"pointofcontact")],
+datetime.date.today(),
+"el",
+u"Title",
+[u"12314213123"],
+u"abstracttttttt",
+["http://www.google.com"],
+["el"],
+["biota"],
+[{'terms':[],'value':"gemet_inspire_data_themes",'name':"Inspire Data Themes",'date':datetime.date(2012,1,1),'datetype':'creation'}], 
+[GeographicBoundingBox(0.0,0.0,0.0,0.0)], 
+[TemporalExtent(datetime.date(2012,1,1),datetime.date(2013,1,1))],
+datetime.date(2012,1,1),
+datetime.date(2013,1,1),
+datetime.date(2014,1,1),
+u"lineaage",
+[0,1,2],
+[SpatialResolution(0,u"meters")],
+[Conformity(u"specifications blabla",datetime.date.today(),"creation","conformant")],
+[u"lalala1",u"lalala2"],
+[u"limit1",u"limit2"],
+[ResponsibleParty(u"Org",[u"email@asd.gr"],"pointofcontact")])
+
+print 'input:'  
+psoc.print_fields()
+errorlist = zope.schema.getValidationErrors(IInspireMetadata,psoc)
+
+if errorlist == []:
+    print 'Validation Success!\n'
+    InspireMetadata.to_xml(psoc,"1.xml")
+else:
+    print 'Validation Failed.\n Errors = %s\n' % errorlist
+
+print 'Test Case #2'
+print 'Test date invariants (upper level)\n'
+
+psoc = InspireMetadata(
+[ResponsibleParty(u"Org",[u"email@asd.gr"],"pointofcontact")],
+datetime.date.today(),
+"el",
+u"Title",
+[u"12314213123"],
+u"abstracttttttt",
+["http://www.google.com"],
+["el"],
+["biota"],
+[{'terms':["lala"],'value':"gemet_inspire_data_themes",'name':"Inspire Data Themes",'date':datetime.date(1000,1,1),'datetype':'creation'}], 
+[GeographicBoundingBox(0.0,0.0,0.0,0.0)], 
+[TemporalExtent(datetime.date(2012,1,1),datetime.date(2013,1,1))],
+datetime.date(2014,1,1),
+datetime.date(2013,1,1),
+datetime.date(2012,1,1),
+u"lineaage",
+[0,1,2],
+[SpatialResolution(0,u"meters")],
+[Conformity(u"specifications blabla",datetime.date.today(),"creation","conformant")],
+[u"lalala1",u"lalala2"],
+[u"limit1",u"limit2"],
+[ResponsibleParty(u"Org",[u"email@asd.gr"],"pointofcontact")])
+
+print 'input:'  
+psoc.print_fields()
+
+errorlist = zope.schema.getValidationErrors(IInspireMetadata,psoc)
+
+if errorlist == []:
+    print 'Validation Success!\n'
+    InspireMetadata.to_xml(psoc,"2.xml")
+else:
+    print 'Validation Failed.\n Errors = %s\n' % errorlist
+
+print 'Test Case #3'
+print 'Test date invariants (lower levels // TemporalExtent + SpatialResolution)\n'
+
+psoc = InspireMetadata(
+[ResponsibleParty(u"Org",[u"email@asd.gr"],"pointofcontact")],
+datetime.date.today(),
+"el",
+u"Title",
+[u"12314213123"],
+u"abstracttttttt",
+["http://www.google.com","http://publicamundi.eu"],
+["el"],
+["biota"],
+[{'terms':["lala1","lala2","lala3"],'value':"gemet_inspire_data_themes",'name':"Inspire Data Themes",'date':datetime.date(2000,1,1),'datetype':'creation'}],
+[GeographicBoundingBox(0.0,0.0,0.0,0.0)], 
+[TemporalExtent(datetime.date(2014,1,1),datetime.date(2012,1,1))],
+datetime.date(2012,1,1),
+datetime.date(2013,1,1),
+datetime.date(2014,1,1),
+u"lineaage",
+[0,1,2,3],
+[SpatialResolution(None,u"meters")],
+[Conformity(u"specifications blabla",datetime.date.today(),"creation","conformant")],
+[u"lalala1",u"lalala2"],
+[u"limit1",u"limit2"],
+[ResponsibleParty(u"Org",[u"email@asd.gr"],"pointofcontact"),ResponsibleParty(u"Org2",[u"email2@asd.gr"],"pointofcontact")])
+
+print 'input:'  
+psoc.print_fields()
+
+errorlist = zope.schema.getValidationErrors(IInspireMetadata,psoc)
+if errorlist == []:
+    print 'Validation Success!\n'
+    InspireMetadata.to_xml(psoc,"3.xml")
+
+    print 'reading xml 3.xml'
+    imd = InspireMetadata.from_xml("3.xml")
+    print 'Inspire model ='
+    print InspireMetadata.print_fields(imd)
+    errorlist = zope.schema.getValidationErrors(IInspireMetadata,imd)
+    if errorlist == []:
+        print 'Validation Success!\n'
+    else:
+        print 'Validation Failed, %s' % errorlist
+
+else:
+    print 'Validation Failed.\n Errors = %s\n' % errorlist
+
+
+
+
+
