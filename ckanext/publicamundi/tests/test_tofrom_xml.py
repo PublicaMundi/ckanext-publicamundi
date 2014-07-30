7bf62bc5c94e2637f89269d2b335a537112bd289
drmalex07 <alexakis@imis.athena-innovation.gr>
2014-07-30 19:41:27 -0400

    Update INSPIRE tests and fixtures

--- ckanext/publicamundi/tests/test_tofrom_xml.py
+++ ckanext/publicamundi/tests/test_tofrom_xml.py
@@ -12,53 +12,12 @@ from ckanext.publicamundi.lib.metadata.base import *
 from ckanext.publicamundi.tests.helpers import assert_faulty_keys
 from ckanext.publicamundi.tests import fixtures
 
-insp1 = InspireMetadata(
-    contact = [
-        ResponsibleParty(organization=u"Org", email=[u"email@asd.gr"], role="pointofcontact")],
-    datestamp = datetime.date.today(),
-    languagecode = "el",
-    title = u"Title",
-    identifier = [u"12314213123"],
-    abstract = u"abstracttttttt",
-    locator = ["http://publicamundi.eu", "http://www.google.com", "http://www.ipsyp.gr", "http://www.example.com"],
-    resource_language = ["el"],
-    topic_category = ["biota"],
-    keywords = [
-        ThesaurusTerms(
-            terms=["air", "agriculture", "climate"],
-            thesaurus=thesaurus_gemet_themes
-        ),    
-        ThesaurusTerms(
-            terms=["buildings", "addresses"],
-            thesaurus=thesaurus_gemet_inspire_data_themes,
-        ),
-    ],
-    bounding_box = [
-        GeographicBoundingBox(nblat=0.0, sblat=0.0, wblng=0.0, eblng=0.0)],
-    temporal_extent = [
-        TemporalExtent(start=datetime.date(2012,1,1), end=datetime.date(2014,1,1))],
-    creation_date = datetime.date(2012,1,1),
-    publication_date = datetime.date(2012,1,1),
-    revision_date = datetime.date(2014,1,1),
-    lineage = u"lineaage",
-    denominator = [],
-    spatial_resolution = [
-        SpatialResolution(distance=5, uom=u"meters")],
-    conformity = [
-        Conformity(title=u"specifications blabla", date=datetime.date.today(), date_type="creation", degree="conformant")],
-    access_constraints = [u"lalala1", u"lalala2"],
-    limitations = [u"limit1", u"limit2"],
-    responsible_party = [
-        ResponsibleParty(organization=u"Org", email=[u"email@asd.gr"], role="pointofcontact"), 
-        ResponsibleParty(organization=u"Org2", email=[u"email2@asd.gr"], role="pointofcontact")]
-)
-
 # Tests
 
 @nose.tools.nottest
 def test_to_xml():
     yield export_to_xml, fixtures.foo1, '/tmp/out-foo-1.xml'
-    yield export_to_xml, insp1, '/tmp/out-inspire-1.xml'
+    yield export_to_xml, fixtures.inspire1, '/tmp/out-inspire-1.xml'
 
 @nose.tools.nottest
 def test_from_xml():
