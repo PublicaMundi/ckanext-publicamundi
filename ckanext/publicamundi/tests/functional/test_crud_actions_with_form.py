3c735e3adeff28eb2e5ff2b3db7ae9831ad03420
smanousopoulos@gmail.com <smanousopoulos@gmail.com>
2014-09-26 22:58:21 +0300

    added 2 functional tests to check validation errors

--- ckanext/publicamundi/tests/functional/test_crud_actions_with_form.py
+++ ckanext/publicamundi/tests/functional/test_crud_actions_with_form.py
@@ -6,6 +6,7 @@ import nose.tools
 import inflection
 import datadiff
 import logging
+import copy 
 
 from datadiff.tools import assert_equal
 
@@ -49,10 +50,13 @@ class TestController(BaseTestController):
     def test_1_create_package(self):
         
         yield self._create_package, 'hello-foo-1'
-    def test_2_update_package(self):
+        yield self._create_package_witherr, 'hello-foo-2', 'wrong1'
     
+    def test_2_update_package(self):
+        
         yield self._update_package, 'hello-foo-1', '0..1'
         yield self._update_package, 'hello-foo-1', '1..2'
+        yield self._update_package_witherr, 'hello-foo-2', 'wrong2'
      
     def test_3_create_resource(self):
     
@@ -150,11 +154,153 @@ class TestController(BaseTestController):
 
         for k in (self.basic_fields & set(res_dict.keys())):
             assert res_dict[k] == pkg_dict[k]
-        
         pkg_dt_dict = dictization.flatten(pkg_dict.get(dt))
         res_dt_dict = dictization.flatten(res_dict.get(dt))
         for k in pkg_dt_dict.keys():
             assert res_dt_dict[k] == pkg_dt_dict[k]
+
+        return
+
+
+    def _create_package_witherr(self, fixture_name, changeset):
+        
+        #pkg_dict = package_fixtures[fixture_name]['0'] 
+        pkg_dict = package_fixtures[fixture_name]['0'] 
+        wrng_pkg_dict = package_fixtures[fixture_name][changeset] 
+        pkg_name = pkg_dict['name']
+
+        res1 = self.app.get('/dataset/new', status='*')
+        assert res1.status == 200
+        
+        dt = pkg_dict['dataset_type']
+        dt_spec = dataset_types[dt]
+        key_prefix = dt_spec.get('key_prefix', dt)
+
+        # 1st stage
+    
+        form1 = res1.forms['package-form'] 
+
+        for k in ['title', 'name', 'dataset_type', 'notes', 'license_id']:
+            v = pkg_dict.get(k)
+            v = v.encode('utf-8') if isinstance(v, unicode) else v
+            form1.set(k, v)
+        form1.set('tag_string', ','.join(map(lambda t: t['name'], pkg_dict.get('tags', []))))
+        
+        res1s = form1.submit('save')
+        assert res1s.status in [301, 302] 
+        
+        # 2nd stage
+
+        res2 = res1s.follow()
+        
+        resource_dict = next(iter(pkg_dict['resources'])) # 1st resource
+        form2 = res2.forms['resource-form']
+        for k in ['description', 'url', 'name', 'format']:
+            v = resource_dict.get(k)
+            v = v.encode('utf-8') if isinstance(v, unicode) else v
+            form2.set(k, v)
+        
+        btns = form2.fields.get('save')
+        i2 = next(j for j, b in enumerate(btns) if b.id == 'btn-save-go-metadata')
+        res2s = form2.submit('save', index=i2, status='*')
+        assert res2s.status in [301, 302]
+
+        # 3rd stage - core metadata
+
+        res3 = res2s.follow()
+        
+        form3 = res3.forms['package-form']
+        for k in ['version', 'url', 'author', 'author_email', 'maintainer', 'maintainer_email']:
+            v = wrng_pkg_dict.get(k)
+            v = v.encode('utf-8') if isinstance(v, unicode) else v
+            form3.set(k, v)
+
+        # 3rd stage - dataset_type-related metadata
+
+        for t, v in dictization.flatten(wrng_pkg_dict.get(dt)).items():
+
+            k = '.'.join((key_prefix,) + tuple(map(str,t)))
+            v = v.encode('utf-8') if isinstance(v, unicode) else v
+            form3.set(k, v)
+
+        btns = form3.fields.get('save')
+        i3 = next(j for j, b in enumerate(btns) if b.id == 'btn-save-finish')
+        res3s = form3.submit('save', index=i3, status='*')
+        #assert res3s.status in [301, 302]
+       
+       # validation error leads to code 200 instead of redirect
+        #assert res3s.status in [200]
+        assert res3s.status in [200]
+        
+        form4 = res3s.forms['package-form']
+        
+        #readd already previous correct fields
+        # TODO bug?
+        for k in ['version', 'url', 'author', 'author_email', 'maintainer', 'maintainer_email']:
+            if k in wrng_pkg_dict:
+                v = wrng_pkg_dict.get(k)
+            else:
+                v = pkg_dict.get(k)
+            v = v.encode('utf-8') if isinstance(v, unicode) else v
+            form4.set(k, v)
+
+        for t, v in dictization.flatten(wrng_pkg_dict.get(dt)).items():
+            k = '.'.join((key_prefix,) + tuple(map(str,t)))
+            v = v.encode('utf-8') if isinstance(v, unicode) else v
+            form4.set(k, v)
+
+        for t, v in dictization.flatten(pkg_dict.get(dt)).items():
+            k = '.'.join((key_prefix,) + tuple(map(str,t)))
+            v = v.encode('utf-8') if isinstance(v, unicode) else v
+            form4.set(k, v)
+
+        # make sure form is built with all foo elements
+        for fld in form4.fields.keys():
+            if fld.startswith('foo'):
+                key = fld.split('.')[1]
+                assert key in pkg_dict.get(dt)
+        
+        #form4.set('foo.baz', u'BaoAb')
+
+        # fill in the correct value
+        #form4.set('foo.'+field, correct_val)
+        
+        # update dict
+        #pkg_dict['foo'][field] = correct_val
+        #print form3
+
+        res3s = form4.submit('save', index=i3, status='*')
+        assert res3s.status in [301, 302]
+
+        # Finished, return to "view" page
+        
+        res4 = res3s.follow()
+        assert res4.status in [200]
+        assert res4.request.url == '/dataset/%s' %(pkg_name)
+
+        # Compare to package_show result
+
+        res_dict = self._get_package(pkg_name)
+
+        assert res_dict['dataset_type'] == dt
+        for k in (self.basic_fields & set(res_dict.keys())):
+            if k in pkg_dict:
+                print res_dict[k]
+                print pkg_dict[k]
+                assert res_dict[k] == pkg_dict[k]
+            else:
+                print res_dict[k]
+                print wrng_pkg_dict[k]
+                assert res_dict[k] == wrng_pkg_dict[k]
+
+        pkg_dt_dict = dictization.flatten(pkg_dict.get(dt))
+        wrng_pkg_dt_dict = dictization.flatten(wrng_pkg_dict.get(dt))
+        res_dt_dict = dictization.flatten(res_dict.get(dt))
+        for k in pkg_dt_dict.keys():
+            if k in pkg_dt_dict:
+                assert res_dt_dict[k] == pkg_dt_dict[k]
+            else:
+                assert res_dt_dict[k] == wrng_pkg_dt_dict[k]
 
         return
 
@@ -197,6 +343,86 @@ class TestController(BaseTestController):
         res2 = res1s.follow()
         assert res2.status in [200]      
         assert res2.request.url == '/dataset/%s' %(pkg_name)
+
+    def _update_package_witherr(self, fixture_name, changeset):
+ 
+        assert fixture_name in package_fixtures
+        pkg_dict = package_fixtures[fixture_name]['0']
+        wrng_pkg_dict = package_fixtures[fixture_name][changeset]
+        
+        pkg_name = package_fixtures[fixture_name]['0']['name']
+        
+        res1 = self.app.get('/dataset/edit/%s' % pkg_name)
+        assert res1.status == 200
+        
+        dt = package_fixtures[fixture_name]['0']['dataset_type']
+        dt_spec = dataset_types[dt]
+        key_prefix = dt_spec.get('key_prefix', dt)
+
+        # Edit core metadata
+        
+        form1 = res1.forms['package-form']
+        for k in self.basic_fields :
+            v = wrng_pkg_dict.get(k)
+            if v is not None:
+                v = v.encode('utf-8') if isinstance(v, unicode) else v
+                form1.set(k, v)
+        if 'tags' in pkg_dict:
+            form1.set('tag_string', ','.join(map(lambda t: t['name'], wrng_pkg_dict.get('tags', []))))
+        
+        # Edit dataset_type-related metadata
+
+        for t, v in dictization.flatten(wrng_pkg_dict.get(dt)).items():
+            k = '.'.join((key_prefix,) + t)
+            v = v.encode('utf-8') if isinstance(v, unicode) else v
+            form1.set(k, v)
+            print k
+            print v
+        
+        # Submit
+
+        res1s = form1.submit('save', status='*')
+        #assert res1s.status in [301, 302]
+
+        # validation error leads to code 200 instead of redirect
+        assert res1s.status in [200]
+        form2 = res1.forms['package-form']
+        for k in self.basic_fields :
+            v = pkg_dict.get(k)
+            if v is not None:
+                v = v.encode('utf-8') if isinstance(v, unicode) else v
+                form2.set(k, v)
+        if 'tags' in pkg_dict:
+            form2.set('tag_string', ','.join(map(lambda t: t['name'], pkg_dict.get('tags', []))))
+        
+        # Edit dataset_type-related metadata
+
+        for t, v in dictization.flatten(pkg_dict.get(dt)).items():
+            k = '.'.join((key_prefix,) + t)
+            v = v.encode('utf-8') if isinstance(v, unicode) else v
+            form2.set(k, v)
+
+        # make sure form is built with all foo elements
+        for fld in form2.fields.keys():
+            if fld.startswith('foo'):
+                key = fld.split('.')[1]
+                assert key in pkg_dict.get(dt)
+        
+        # fill in the correct value
+        #form2.set('foo.'+field, correct_val)
+        
+        # update dict
+        #pkg_dict['foo'][field] = correct_val
+
+# Submit
+
+        res1s = form1.submit('save', status='*')
+        assert res1s.status in [301, 302]
+        
+        res2 = res1s.follow()
+        assert res2.status in [200]      
+        assert res2.request.url == '/dataset/%s' %(pkg_name)
+
 
     def _delete_package(self, fixture_name):
  
@@ -430,7 +656,6 @@ resource_fixtures['resource-3'] = {
 }
 
 package_fixtures = {}
-
 package_fixtures['hello-foo-1'] = {
     '0': {
         'title': u'Hello Foo (1)',
@@ -450,7 +675,7 @@ package_fixtures['hello-foo-1'] = {
         ],
         'dataset_type': 'foo',
         'foo': {
-            'baz': u'shoort',
+            'baz': u'BaoAb',
             'rating': 9,
             'grade': 5.12,
             'reviewed': False,
@@ -486,3 +711,65 @@ package_fixtures['hello-foo-1'] = {
         }, 
     },
 }
+
+package_fixtures['hello-foo-2'] = {
+    '0': {
+        'title': u'Hello Foo (2)',
+        'name': 'hello-foo-2',
+        'notes': u'I am the second _Foo_ dataset!',
+        'author': u'Λαλάκης',
+        'license_id': 'gfdl',
+        'version': '1.0.0',
+        'maintainer': u'Nowhere Man',
+        'author_email': 'lalakis.1999@example.com',
+        'maintainer_email': 'nowhere-man@example.com',
+        'url': 'http://example.com/datasets/hello-foo-2',
+        'tags': [ 
+            { 'name': 'hello-world', 'display_name': 'Hello World', }, 
+            { 'name': u'test', 'display_name': 'Test' }, 
+            { 'name': 'foo', 'display_name': 'Foo' }, 
+        ],
+        'dataset_type': 'foo',
+        'foo': {
+            'baz': u'bzzzz',
+            'rating': 9,
+            'grade': 5.12,
+            'reviewed': False,
+            'created': u'2014-09-13T17:00:00',
+            'temporal_extent': {
+                'start': '2012-01-01',
+                'end': '2013-01-01',
+            },
+            'thematic_category': 'health',
+        },
+        'resources': [
+            resource_fixtures['resource-1']['0'],
+        ],
+    },
+    '': {
+        'foo': {
+            'baz': u'Ενα δενδρο BaoBab',
+            'grade': 6.89,
+            'reviewed': True,
+            'thematic_category': 'environment',
+        },
+    },
+    'wrong1': {
+        'foo': {
+            'baz': u'B',
+            'grade': 6.89,
+            'reviewed': True,
+            'thematic_category': 'environment',
+        },
+    },
+    'wrong2': {
+        'foo': {
+            'baz': u'B',
+            'grade': 26.89,
+            'reviewed': True,
+            'thematic_category': 'environment',
+        },
+    },
+
+}
+
