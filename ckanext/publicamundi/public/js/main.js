99abf7cecb205d59759d64031d17f378710cd87a
drmalex07 <alexakis@imis.athena-innovation.gr>
2014-10-15 04:46:08 +0300

    Refactored javascript widgets for collection-based fields (use jQuery-Ui widget factory)

--- ckanext/publicamundi/public/js/main.js
+++ ckanext/publicamundi/public/js/main.js
@@ -3,235 +3,352 @@ this.ckanext.publicamundi || (this.ckanext.publicamundi = {})
 
 jQuery(document).ready(function ($) {
     
-    var msg_prefix = '[publicamundi]'
     var console = window.console
-
-    var debug = function () {
-        console.debug.apply(console, [msg_prefix].concat(Array.slice(arguments)))
-    }
-    var error = $.proxy(console, 'error')
+    var debug = $.proxy(console, 'debug') 
     var warn = $.proxy(console, 'warn')
+    var error = $.proxy(console, 'error')
     var assert = $.proxy(console, 'assert')
    
-    var Name = function (qname) {
-        var i = qname.toString().lastIndexOf('.')
-        this.qname = qname
-        this.namePrefix = (i > 0)? qname.substring(0, i) : null
-        this.nameKey = (i > 0)? qname.substring(i + 1) : qname
+    var parseName = function (qname) {
+        var i = qname.lastIndexOf('.')
+        return {
+            qname: qname,
+            namePrefix: (i > 0)? qname.substring(0, i) : null,
+            nameKey: (i > 0)? qname.substring(i + 1) : qname,
+        }
     }
-    
-    Name.parse = function (qname) { return new Name(qname) }
 
-    var FieldWidget = $.inherit(Object, 
-        // Define instance (prototype) properties
+    $.widget('publicamundi.fieldwidget', {
+        options: {
+            qname: null,
+            title: null,
+            template: null,
+            defaultTemplate: function (name) {
+                return 'script#' + name.qname.replace(/[.]/g, '\\.') + '-template' 
+            },
+            classPrefix: 'field-qname-',
+            hide: { duration: 400, },
+            show: { duration: 400, easing: 'swing' },
+        },
+
+        _create: function()
         {
-            name: null,
-            _isAttached: null,
-            _isPrepared: false,
-            constructor: function (qname, $el, $parent) 
-            {
-                this.name = Name.parse(qname)
-                this.$el = $el
-                
-                if ($parent) {
-                    this.$parent = $parent
-                    this._isAttached = false
+            var opts = this.options
+            
+            this.name = parseName(opts.qname)
+            this.title = opts.title
+            
+            if (opts.template) {
+                // The field widget doesnt exist: must be created from a template 
+                var $w = null, tpl = null, dat = null
+                if ($.isPlainObject(opts.template)) {
+                    tpl = opts.template.source || 'default'
+                    dat = opts.template.data
+                    dat = $.isEmptyObject(dat)? {} : Object.create(dat)
                 } else {
-                    $parent = $el.parent()
-                    if ($parent.length) {
-                        this.$parent = $parent
-                        this._isAttached = true
-                    } else {
-                        throw new Error('Cannot create a widget without a parent!')
+                    tpl = opts.template.toString()
+                    dat = {}
+                }
+                if (tpl instanceof jQuery) {
+                    tpl = tpl.text()
+                } else {
+                    tpl = $.trim(tpl.toString())
+                    if (tpl.lastIndexOf('<', 0) != 0) {
+                        // assume it's a selector
+                        if (tpl == 'default')
+                            tpl = opts.defaultTemplate(this.name)  
+                        tpl = $(tpl).text()
                     }
                 }
-            },
-            prepare: function () 
-            {
-                if (!this._isPrepared) {
-                    this._prepareOnce()
-                    this._isPrepared = true
-                }
-            },
-            render: function (show) 
-            {
-                show = (show == null)? true : Boolean(show)
-                if (show) {
-                    this.$el.fadeIn()
-                } else {
-                    this.$el.fadeOut()
-                }
-            },
-            _prepareOnce: function()
-            {
-                if (!this._isAttached) {
-                    this.$el.hide()
-                    this.$parent.append(this.$el)
-                }
+                // Render template and verify result
+                $.extend(dat, this._getTemplateVars())
+                $w = $(Mustache.render(tpl, dat))
+                assert($w.hasClass('field-widget'))
+                assert($w.hasClass(opts.classPrefix + this.name.qname))
+                // Attach to container element
+                this.element.empty().append($w)
+                this.$widget = $w
+            } else {
+                // The field widget allready exists
+                var selector = '.widget' 
+                    + ('.' + opts.classPrefix + this.name.qname.replace(/[.]/g, '\\.'))
+                this.$widget = this.element.children(selector)
+                assert(this.$widget.length == 1) 
+            }
+            
+            if (!this.title) {
+                this.title = this._extractTitle()
+            }
+
+            // If created at disabled mode, disable it. Fire the appropriate
+            // events in any case.
+            
+            this._trigger('created')
+            
+            if (opts.disabled) {
+                this._disable()
+                this._trigger('disabled')
+                debug('Created widget "' + this.widgetFullName + '" at disabled mode')
+            } else { 
+                this._trigger('enabled')
+            }
+
+            // Done
+
+            this._refresh()
+        },
+
+        _refresh: function()
+        {
+            $.noop()
+        },
+
+        destroy: function()
+        {
+            // Cleanup events and attributes.
+            // Note: It will delegate to _destroy() for widget-specific cleanup
+            this._super()
+            // When finished, fire a proper event
+            this._trigger('destroyed')
+        },
+
+        _destroy: function()
+        {
+            var opts = this.options
+            
+            if (opts.template) {
+                this.element.empty()
             }
         },
-        // Define static properties
-        {
-            clsPrefix: 'field-qname-',
-            createFromElement: function ($el) 
-            {
-                var ctor = this, qname = null, widget = null
-                qname = $el.attr('class').split(' ')
-                    .filter(function (a) { return a.startsWith(ctor.clsPrefix) })
-                if (qname.length > 0) {
-                    qname = qname.pop().substring(ctor.clsPrefix.length) 
-                    widget = new ctor(qname, $el)
-                }    
-                return widget
-            },
-            createFromName: function (qname) 
-            {
-                var ctor = this, widget = null
-                var selector = '.widget' + ('.' + ctor.clsPrefix + qname.replace(/[.]/g, '\\.'))
-                var $el = $(selector).first()
-                if ($el.length > 0) {
-                    widget = new ctor(qname, $el)
-                }
-                return widget
-            },
-            createFromTemplate: function (tpl, $parent, qname, data) 
-            {
-                var ctor = this, widget = null, $el = null
-                
-                // Override data (via prototype) and render template
-                data = Object.create(data)
-                data.qname = qname
-                $el = $(Mustache.render(tpl, data))
-                
-                // Verify proper classes exist
-                assert($el.hasClass('field-widget'))
-                assert($el.hasClass(ctor.clsPrefix + qname)) 
-                
-                // Create widget
-                widget = new ctor(qname, $el, $parent)
-                return widget
-            }, 
-            createFromTemplateScript: function ($tpl, $parent, qname, data)
-            {
-                var ctor = this, tpl = null
-                if (!($tpl instanceof jQuery)) {
-                    $tpl = $('script#' 
-                        + qname.replace(/[.]/g, '\\.') + '-template')
-                }
-                if (!$tpl.length) {
-                    return null
-                } else {
-                    tpl = $tpl.text()
-                    return ctor.createFromTemplate(tpl, $parent, qname, data)
-                }
-            },
-        }
-    );
-    
-    var ContainerItemEditor = $.inherit(FieldWidget, 
-        {
-            constructor: function (qname, $el, $parent) {
-                FieldWidget.call(this, qname, $el, $parent)
-            }, 
+        
+        _setOptions: function() 
+        { 
+            this._superApply(arguments);
+            this._refresh();
         },
+
+        _setOption: function (key, value)
         {
-            createFromTemplate: function (tpl, $parent, qname, data) 
-            {
-                var ctor = this, parent_ctor = FieldWidget
-                qname = Name.parse(qname)
-                data = Object.create(data)
-                data.key = qname.nameKey
-                return parent_ctor.createFromTemplate.call(ctor, tpl, $parent, qname.qname, data) 
-            },            
-            createFromTemplateScript: function ($tpl, $parent, qname, data)
-            {
-                var ctor = this, parent_ctor = FieldWidget, tpl = null
-                qname = Name.parse(qname)
-                data = Object.create(data)
-                data.key = qname.nameKey
-                if (!($tpl instanceof jQuery)) {
-                    $tpl = $('script#' 
-                        + qname.namePrefix.replace(/[.]/g, '\\.') + '-item-template')
-                }
-                if (!$tpl.length) {
-                    return null
-                } else {
-                    tpl = $tpl.text()
-                    return parent_ctor.createFromTemplate.call(ctor, tpl, $parent, qname.qname, data)
-                }
-            },
-        }
-    );
-    
-    var ListItemEditor = $.inherit(ContainerItemEditor, 
-        {
-            // Todo
+            switch (key) {
+                case 'disabled':
+                    warn('You can enable/disable the "' + this.widgetFullName + 
+                        '" widget using the namesake methods')
+                    break;
+                case 'qname':
+                case 'template':
+                case 'classPrefix':
+                    warn('The option (' + key + ') ' +
+                        'cannot be changed after widget\'s creation')
+                    break;
+                default:
+                    this._super(key, value);
+                    break;
+            }
         },
-        {
-            // Todo
-        }
-    );
-    
-    var DictItemEditor = $.inherit(ContainerItemEditor, 
-        {
-            render: function (show) 
-            {
-                show = (show == null)? true : Boolean(show)
 
-                if (show) {
-                    // ..
-                } else {
-                    // ..
-                }
-            },
-            _prepareOnce: function () 
-            {
-                // Add a "remove" button, re-style header
-                
-                var widget = this, ctor = this.constructor
-                var $w1 = widget.$el.children('.object-edit-widget')
-                var $header = $w1.children('header')
-            
-                var $remove_btn = $(Mustache.render(
-                    ctor.templates['remove-btn'], { label: 'Remove' }))
-                $remove_btn
-                    .on('click', function () {
-                        widget.render(false)
-                    })
-                    .on('mouseover', function () { 
-                        widget.$el.css({'background-color': '#f7f7f7',})
-                    }) 
-                    .on('mouseout', function () { 
-                        widget.$el.css({'background-color':'inherit',}) 
-                    }) 
-                
-                $header.children('h3').addClass('inline')
-                $header.append($remove_btn)
-                
-                // Create a placeholder widget
-
-                // Todo
-            },
+        enable: function()
+        {
+            if (this.options.disabled) {
+                this._enable()
+                this.options.disabled = false
+                this._trigger('enabled')
+            }
         },
+       
+        disable: function()
         {
-            options: {
-                showPlaceholder: true,
-            },
-            templates: {
-                'remove-btn': 
-                    '<a class="btn btn-small pull-right remove-object" title="{{title}}"><i class="icon-remove"></i> {{label}}</a>',
-                'edit-btn':
-                    '<a class="btn btn-small pull-right edit-object" title="{{title}}"><i class="icon-pencil"></i> {{label}}</a>',
-                'placeholder-widget':
-                    '<header><h3 class="inline">{{title}}</h3><span class="label not-available">n/a</span> {{{edit-btn}}}</header>',
-            },
-        }
-    );
+            if (!this.options.disabled) {
+                this._disable()
+                this.options.disabled = true
+                this._trigger('disabled')
+            }
+        },
 
-    $.extend(ckanext.publicamundi, {
-        FieldWidget: FieldWidget,
-        ListItemEditor: ListItemEditor,
-        DictItemEditor: DictItemEditor,
+        // Implementation
+
+        _enable: function() 
+        { 
+            this._show(this.element, this.options.show)
+            //this.element.show(this.options.show)
+        },
+
+        _disable: function() 
+        { 
+            this._hide(this.element, this.options.hide)
+            //this.element.hide(this.options.hide)    
+        },
+
+        _getTemplateVars: function() 
+        { 
+            var vars = $.extend({}, this.name)
+            if (this.title) {
+                vars.title = this.title
+            }
+            return vars
+        },
+
+        _extractTitle: function()
+        {
+            // Try to extract title from contained widget
+            var title = null 
+            var $header = this.$widget.children('header')
+            if ($header.length) {
+               title = $header.children('h1,h2,h3,h4,h5').text()
+            }
+            return title
+        },
+    }) 
+    
+    $.widget('publicamundi._itemEditor', $.publicamundi.fieldwidget, {
+        options: {
+            defaultTemplate: function (name) { 
+                return 'script#' 
+                    + name.namePrefix.replace(/[.]/g, '\\.') + '-item-template' 
+            }
+        },
+        
+        _destroy: function()
+        {
+            // Empty everything, even if not created from template
+            this.element.empty()
+        },
+
+        _enable: function()
+        {
+            this.$widget.find(':input').attr('disabled', null)
+        },
+       
+        _disable: function()
+        {
+            this.$widget.find(':input').attr('disabled', 'disabled') 
+        },
+       
+        _getTemplateVars: function() 
+        { 
+            var vars = this._super()
+            return $.extend(vars, { key: this.name.nameKey })
+        },
+
+        _extractTitle: function()
+        {
+            var title = null 
+            var $w1 = this.$widget.children('.object-edit-widget')
+            var $header = $w1.children('header')
+            if ($header.length) {
+               title = $header.children('h1,h2,h3,h4,h5').text()
+            }
+            return title
+        },
+
     })
+   
+    $.widget('publicamundi.itemEditor', $.publicamundi._itemEditor, {
+        widgetEventPrefix: 'publicamundi-item_editor-',
+        
+        options: {
+            placeholder: true, // Use a placeholder when detached (disabled mode)
+            editorTemplates: {
+                removeBtn:
+                    '<a class="btn btn-small pull-right" title="{{title}}"><i class="icon-remove"></i> {{label}}</a>',
+                editBtn:
+                    '<a class="btn btn-small pull-right" title="{{title}}"><i class="icon-pencil"></i> {{label}}</a>',
+                placeholder:
+                    '<h3 class="inline">{{title}}</h3><span class="label not-available">n/a</span>',
+            },
+        },
+
+        _create: function()
+        {
+            this._super()
+            this._prepareEditor()
+        },
+        
+        _prepareEditor: function()
+        {
+            // Add a "remove" button, re-style header
+                
+            var templates = this.options.editorTemplates, $widget = this.$widget
+            var $remove_btn, $edit_btn, $p, remove_handler, edit_handler
+            
+            var $w1 = $widget.children('.object-edit-widget')
+            var $header = $w1.children('header')
+
+            $remove_btn = $(Mustache.render(templates.removeBtn, { label: 'Remove' }))
+            $remove_btn.addClass('remove-object')
+            
+            $header.children('h3').addClass('inline')
+            $header.append($remove_btn)
+            
+            $edit_btn = $(Mustache.render(templates.editBtn, { label: 'Edit' }))
+            $edit_btn.addClass('edit-object')
+                       
+            if (this.options.placeholder) {
+                // Create a placeholder element, setup enable/disable handlers
+                $p = $(Mustache.render(templates.placeholder, { title: this.title }))
+                $p = $p.appendTo($('<header/>')).parent()
+                this.$placeholder = $p.append($edit_btn)    
+                edit_handler = function () { this.enable() }
+                remove_handler = function () { this.disable() }
+            } else {
+                // No placeholder, just setup enable/disable handlers
+                edit_handler = function () { 
+                    this.enable()
+                    $edit_btn.detach()
+                    $header.append($remove_btn)
+                } 
+                remove_handler = function () { 
+                    this.disable()
+                    $remove_btn.detach()
+                    $header.append($edit_btn)
+                }
+            }
+
+            // Bind event handlers
+                 
+            this._on(true, this.element, {
+                'click header > .edit-object': edit_handler
+            })
+
+            this._on(false, this.element, {
+                'click header > .remove-object': remove_handler
+            })
+
+            this._on(false, this.element, {
+                'mouseover header > .remove-object': function () {
+                    this.$widget.css({'background-color': '#f7f7f7',})
+                },
+                'mouseout header > .remove-object': function () {
+                    this.$widget.css({'background-color': 'inherit',})
+                }, 
+            })
+
+            return
+        },
+
+        _enable: function()
+        {
+            if (this.options.placeholder) {
+                this.$placeholder.detach()
+                this.$widget.hide()
+                this.element.append(this.$widget)
+                this.$widget.fadeIn(this.options.show)
+            } else {
+                this.$widget.find(':input').attr('disabled', null)
+            }
+        },
+       
+        _disable: function()
+        {
+            if (this.options.placeholder) {
+                this.$widget.detach()
+                this.$placeholder.hide()
+                this.element.append(this.$placeholder)
+                this.$placeholder.fadeIn(this.options.show)
+            } else {
+                this.$widget.find(':input').attr('disabled', 'disabled')
+            }
+        },
+    })
+    
 });
 
--- ckanext/publicamundi/public/js/modules/dataset-foo.js
+++ ckanext/publicamundi/public/js/modules/dataset-foo.js
@@ -5,126 +5,36 @@ this.ckan.module('input-contacts-foo', function ($, _) {
         options: {
             qname: null,
             terms: null,
-            titles: null,
         },
+
         initialize: function () 
         {
-            var module = this, qname = this.options.qname, $el = this.el
-            
+            var module = this, opts = this.options, qname = opts.qname, $el = this.el
             var $list = $el.children('ul')
-            var $existing_items = $list.children('li')   
-            var existing_keys = $existing_items
-                .map(function (i, y) { return $(y).data('key') })
-                .get()
-
-            module.keys = {}
-            for (k in module.options.terms) {
-                if ($.inArray(k, existing_keys) >= 0) {
-                    module.keys[k] = true
-                    module._prepareEditorWidget(k)
-                } else {
-                    var $placeholder = module._createPlaceholderWidget(k)
-                    $('<li>').data('key', k)
-                        .append($placeholder)
-                        .appendTo($list)
-                    module.keys[k] = false
+            
+            for (var key in opts.terms) {
+                var yopts = { 
+                    qname: qname + '.' + key, 
+                    title: opts.terms[key].title
                 }
+                var $y = $list.children('li')
+                    .filter(function () { return $(this).data('key') == key })
+                if ($y.length) {
+                    // The item allready exists
+                    $.noop()
+                } else {
+                    // No item: create a <li> container, use the templated editor
+                    $y = $('<li/>').data('key', key).appendTo($list)
+                    $.extend(yopts, { template: 'default' })
+                }
+                $y.itemEditor(yopts)
             }
-           
-            var $item_tpl = $el.find(
-                'script#' + qname.replace(/[.]/g, '\\.') + '-item-template')
-            module.templates['editor-widget'] = $item_tpl.text()
-
+            
             window.console.log('Initialized module: input-contacts-foo')
         },
+
         teardown: function () { 
             window.console.log('Tearing down module: input-contacts-foo')
-        },
-        templates: {
-            'remove-btn': 
-                '<a class="btn btn-small pull-right remove-object" title="{{title}}"><i class="icon-remove"></i> {{label}}</a>',
-            'edit-btn':
-                '<a class="btn btn-small pull-right edit-object" title="{{title}}"><i class="icon-pencil"></i> {{label}}</a>',
-            'placeholder-widget':
-                '<div><header><h3 class="inline">{{title}}</h3><span class="label not-available">n/a</span> {{{edit-btn}}}</header></div>',
-        },
-        
-        // Helpers
-        _renderTemplate: function (name, data)
-        {
-            return Mustache.render(this.templates[name], data)
-        },
-        _createPlaceholderWidget: function (key)
-        {
-            var module = this, qname = this.options.qname, $el = this.el
-            
-            var $placeholder_widget = $(module._renderTemplate('placeholder-widget', {
-                'title': module.options.terms[key].title,
-                'edit-btn': module._renderTemplate('edit-btn', { 
-                    label: 'Create', title: 'Provide info for this contact',
-                }),
-            }))
-
-            var $edit_btn = $placeholder_widget.find('header > .btn')
-            $edit_btn
-                .on('click', function () { 
-                    var $editor_widget = module._createEditorWidget(key)
-                    $(this).closest('li').empty().append($editor_widget)
-                    module._prepareEditorWidget(key)
-                    module.keys[key] = true
-                    window.console.info('Added an item widget keyed at "'+key+'"')
-                })
-                .on('mouseover', function () { 
-                    $placeholder_widget.css({'background-color': '#f7f7f7',})
-                }) 
-                .on('mouseout', function () { 
-                    $placeholder_widget.css({'background-color':'inherit',}) 
-                }) 
-
-            return $placeholder_widget
-        },
-        _createEditorWidget: function (key)
-        {
-            // Create an editor widget based on the provided (Mustache) template
-            
-            var module = this, qname = this.options.qname, $el = this.el
-            
-            var $editor_widget = module._renderTemplate('editor-widget', {
-                'key': key,
-                'title': module.options.terms[key].title,
-            })
-           
-            return $editor_widget
-        },
-        _prepareEditorWidget: function (key)
-        {
-            var module = this, qname = this.options.qname, $el = this.el
-
-            var $y = $el.children('ul').children('li')
-                .filter(function () { return $(this).data('key') == key })
-            var widget_selector = 'div.widget'
-                + '.object-qname-' + (qname + '.' + key).replace(/[.]/g, '\\.')
-            
-            var $widget = $y.find(widget_selector)
-            var $widget_header = $widget.children('header')
-            var $remove_btn = $(module._renderTemplate('remove-btn', { label: 'Remove' }))
-            $remove_btn
-                .on('click', function () { 
-                    $y.fadeOut(400, 'swing', function () { 
-                        var $placeholder = module._createPlaceholderWidget(key)
-                        $(this).empty().append($placeholder).fadeIn()
-                        module.keys[key] = false
-                        window.console.info('Removed item widget keyed at "'+key+'"')
-                    })
-                })
-                .on('mouseover', function () { 
-                    $widget.css({'background-color': '#f7f7f7',})
-                }) 
-                .on('mouseout', function () { 
-                    $widget.css({'background-color':'inherit',}) 
-                }) 
-            $widget_header.children('h3').addClass('inline')
-            $widget_header.append($remove_btn)
         },
     }
 })
