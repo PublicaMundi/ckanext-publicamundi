d3d596d76f5a34c26ad80aa0c648b24d65b07c8f
drmalex07 <alexakis@imis.athena-innovation.gr>
2014-10-21 14:32:59 +0300

    Created read/write widget for INSPIRE datasets

---
+++ ckanext/publicamundi/lib/metadata/widgets/objects/inspire_metadata.py
@@ -0,0 +1,55 @@
+import zope.interface
+
+from ckanext.publicamundi.lib.metadata import schemata
+from ckanext.publicamundi.lib.metadata.fields import *
+from ckanext.publicamundi.lib.metadata.widgets import (
+    object_widget_adapter, field_widget_adapter, field_widget_multiadapter)
+from ckanext.publicamundi.lib.metadata.widgets import base as base_widgets
+from ckanext.publicamundi.lib.metadata.widgets.base import (
+    EditFieldWidget, EditObjectWidget, 
+    ReadFieldWidget, ReadObjectWidget,
+    ListFieldWidgetTraits, DictFieldWidgetTraits)
+
+@object_widget_adapter(schemata.IInspireMetadata, 
+    qualifiers=['datasetform'], is_fallback=True)
+class InspireEditWidget(EditObjectWidget):
+
+    def prepare_template_vars(self, name_prefix, data):
+        tpl_vars = super(InspireEditWidget, self).prepare_template_vars(name_prefix, data)
+        # Add variables
+        return tpl_vars
+    
+    def get_omitted_fields(self):
+        return [
+            'bounding_box'
+        ]
+
+    def get_field_qualifiers(self):
+        return {
+            # Todo
+        }
+        
+    def get_template(self):
+        return None # use glue template
+
+@object_widget_adapter(schemata.IInspireMetadata)
+class InspireReadWidget(ReadObjectWidget):
+    
+    def prepare_template_vars(self, name_prefix, data):
+        tpl_vars = super(InspireReadWidget, self).prepare_template_vars(name_prefix, data)
+        # Add variables
+        return tpl_vars
+     
+    def get_omitted_fields(self):
+        return [
+            'bounding_box'
+        ]
+  
+    def get_field_qualifiers(self):
+        return {
+            # Todo
+        }
+
+    def get_template(self):
+        return None # use glue template
+
