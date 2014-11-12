6ca26baa0e5c7320a1ec28a9ba4b8aca886809e6
smanousopoulos@gmail.com <smanousopoulos@gmail.com>
2014-11-12 17:39:39 +0200

    initial commit

---
+++ ckanext/publicamundi/themes/geodata/plugin.py
@@ -0,0 +1,16 @@
+import ckan.plugins as plugins
+import ckan.plugins.toolkit as toolkit
+
+class GeodataThemePlugin(plugins.SingletonPlugin):
+    '''An example theme plugin.
+    '''
+    # Declare that this class implements IConfigurer.
+    plugins.implements(plugins.IConfigurer)
+
+    def update_config(self, config):
+
+        # Add this plugin's templates dir to CKAN's extra_template_paths, so
+        # that CKAN will use this plugin's custom templates.
+        toolkit.add_template_directory(config, 'templates')
+        toolkit.add_public_directory(config, 'public')
+        toolkit.add_resource('public', 'ckanext-publicamundi-geodata-theme')
