d7c229a2574579275068553c3bd6683af4d6f5f4
drmalex07 <alexakis@imis.athena-innovation.gr>
2014-11-18 02:50:11 +0200

    Added a dummy named route for `maps` menu item

--- ckanext/publicamundi/themes/geodata/plugin.py
+++ ckanext/publicamundi/themes/geodata/plugin.py
@@ -1,16 +1,16 @@
+import datetime
+
 import ckan.plugins as plugins
 import ckan.plugins.toolkit as toolkit
-import datetime
 
 def most_recent_datasets(limit=10):
     datasets = toolkit.get_action('package_search')(
-            data_dict={'sort': 'metadata_modified desc', 'rows':8})
-
+        data_dict={'sort': 'metadata_modified desc', 'rows':8})
     return datasets
 
 def list_menu_items (limit=16):
     groups = toolkit.get_action('group_list')(
-            data_dict={'sort': 'name desc', 'all_fields':True})
+        data_dict={'sort': 'name desc', 'all_fields':True})
     groups = groups[:limit]
 
     return groups
@@ -20,17 +20,23 @@ def friendly_date(date_str):
     return date.strftime('%d, %b, %Y')
 
 class GeodataThemePlugin(plugins.SingletonPlugin):
-    '''An example theme plugin.
+    '''Theme plugin for geodata.gov.gr.
     '''
-    # Declare that this class implements IConfigurer.
+
     plugins.implements(plugins.IConfigurer)
     plugins.implements(plugins.ITemplateHelpers)
+    plugins.implements(plugins.IRoutes, inherit=True)
     
+    # ITemplateHelpers
+
     def get_helpers(self):
-        return {'newest_datasets': most_recent_datasets,
-                'list_menu_items': list_menu_items,
-                'friendly_date': friendly_date
-            }
+        return {
+            'newest_datasets': most_recent_datasets,
+            'list_menu_items': list_menu_items,
+            'friendly_date': friendly_date
+        }
+    
+    # IConfigurer
 
     def update_config(self, config):
 
@@ -39,3 +45,12 @@ class GeodataThemePlugin(plugins.SingletonPlugin):
         toolkit.add_template_directory(config, 'templates')
         toolkit.add_public_directory(config, 'public')
         toolkit.add_resource('public', 'ckanext-publicamundi-geodata-theme')
+
+    # IRoutes
+
+    def before_map(self, mapper):
+
+        mapper.connect('maps', '') 
+        
+        return mapper
+
