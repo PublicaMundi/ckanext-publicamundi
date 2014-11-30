9604b614052fc9d57d72be1ccb69f3d5b5b983c4
Angelos Tzotsos <gcpp.kalxas@gmail.com>
2014-11-30 23:43:06 +0200

    Defined functions for clean installation and removal of CswRecord (#69)

--- ckanext/publicamundi/model/__init__.py
+++ ckanext/publicamundi/model/__init__.py
@@ -4,5 +4,13 @@ from sqlalchemy.ext.declarative import declarative_base
 Base = declarative_base()
 
 from .csw_record import CswRecord
+from .csw_record import post_setup as csw_post_setup
+from .csw_record import pre_cleanup as csw_pre_cleanup
 
 from .resource_ingest import ResourceIngest
+
+def post_setup(self):
+	csw_post_setup()
+
+def pre_cleanup(self):
+	csw_pre_cleanup()
