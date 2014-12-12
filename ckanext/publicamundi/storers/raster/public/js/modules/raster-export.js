abc9780af9a580ede898c213cc56b484245134dc
Alex Dumitru <dumitru@rasdaman.com>
2014-12-12 18:57:01 +0100

    fixing export

--- ckanext/publicamundi/storers/raster/public/js/modules/raster-export.js
+++ ckanext/publicamundi/storers/raster/public/js/modules/raster-export.js
@@ -3,8 +3,8 @@
         $(".raster-export-button").on("click", function(){
             var request = 'http://labs.geodata.gov.gr/rasdaman/ows/?service=WCS&version=2.0.1&request=ProcessCoverages&query=for c in (coverage_id) return encode(c, "format")'
             var $this = $(this);
-            var coverage_id ="Cov_" + $this.attr("data-resource-id").replace(/-/g, "_");
-            var coverage_format =$this.attr("data-resource-format")
+            var coverage_id =$this.attr("data-resource-id");
+            var coverage_format =$this.attr("data-resource-format");
             var filledRequest = request.replace('coverage_id', coverage_id).replace('format', coverage_format);
             window.open(filledRequest, "_blank")
         })
