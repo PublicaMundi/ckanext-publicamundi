8738f241c44f6ea2729924b89cb10f381d6208c3
smanousopoulos@gmail.com <smanousopoulos@gmail.com>
2015-05-25 16:21:56 +0300

    menu navigation refactoring

--- ckanext/publicamundi/themes/geodata/controllers/static.py
+++ ckanext/publicamundi/themes/geodata/controllers/static.py
@@ -5,21 +5,6 @@ from ckan.lib import helpers
 import ckan.plugins.toolkit as toolkit
 
 class Controller(BaseController):
-    def redirect_news(self):
-        locale = helpers.lang()
-        #return('/news?lang=el')
-        wp_url = config.get('ckanext.publicamundi.themes.geodata.wordpress_url')
-
-        if wp_url:
-            redirect(wp_url+'?lang={0}'.format(locale))
-        else:
-            redirect('/')
-        #redirect('/news?lang={0}'.format(locale))
     def developers(self):
         return render('developers/index.html')
 
-    def redirect_maps(self):
-        base_url = config.get('ckan.site_url')
-        maps_url = config.get('ckanext.publicamundi.themes.geodata.maps_url')
-        if base_url and maps_url:
-            redirect(base_url+maps_url)
--- ckanext/publicamundi/themes/geodata/plugin.py
+++ ckanext/publicamundi/themes/geodata/plugin.py
@@ -2,6 +2,7 @@ import datetime
 
 import ckan.plugins as plugins
 import ckan.plugins.toolkit as toolkit
+from ckan.lib import helpers
 from ckan.lib.base import c
 from ckan.lib.helpers import render_datetime, resource_preview
 import ckanext.publicamundi.lib.template_helpers as ext_template_helpers
@@ -24,9 +25,10 @@ def friendly_date(date_str):
 
 
 _feedback_form = None
+_maps_url = None
+_wp_url = None
 _non_previewable_formats = ['geotiff', 'gml', 'shapefile', 'shp' ]
 _previewable_formats = ['wms', 'wfs']
-
 
 def feedback_form():
     return _feedback_form
@@ -36,6 +38,19 @@ def get_non_previewable_formats():
 
 def get_previewable_formats():
     return _previewable_formats
+
+def get_maps_suffix():
+    if _maps_url:
+        return _maps_url
+    else:
+        return '/'
+
+def get_wp_suffix():
+    locale = helpers.lang()
+    if _wp_url:
+        return(_wp_url+'?lang={0}'.format(locale))
+    else:
+        return '/'
 
 # Returns the most suitable preview by checking whether ingested resources provide a better preview visualization
 def preview_resource_or_ingested(res, pkg):
@@ -79,6 +94,8 @@ class GeodataThemePlugin(plugins.SingletonPlugin):
             'friendly_date': friendly_date,
             'feedback_form': feedback_form,
             'preview_resource_or_ingested': preview_resource_or_ingested,
+            'get_wp_suffix': get_wp_suffix,
+            'get_maps_suffix': get_maps_suffix,
         }
     
     # IConfigurer
@@ -97,9 +114,13 @@ class GeodataThemePlugin(plugins.SingletonPlugin):
         '''Pass configuration to plugins and extensions'''
 
         global _feedback_form
+        global _wp_url
+        global _maps_url
 
         _feedback_form = config.get('ckanext.publicamundi.themes.geodata.feedback_form')
-        
+        _maps_url = config.get('ckanext.publicamundi.themes.geodata.maps_suffix')
+        _wp_url = config.get('ckanext.publicamundi.themes.geodata.wp_suffix')
+
         return
 
     # IRoutes
@@ -108,8 +129,8 @@ class GeodataThemePlugin(plugins.SingletonPlugin):
         mapper.connect('developers', '/developers', controller= 'ckanext.publicamundi.themes.geodata.controllers.static:Controller', action='developers')
         #mapper.connect('maps', '/maps', controller= 'ckanext.publicamundi.themes.geodata.controllers.static:Controller', action='redirect_maps' )
         #mapper.redirect('maps', 'http://http://83.212.118.10:5000/maps')
-        mapper.connect('maps', '/maps')
-        mapper.connect('news', '/news', controller= 'ckanext.publicamundi.themes.geodata.controllers.static:Controller', action='redirect_news' )
+        #mapper.connect('maps', '/maps')
+        #mapper.connect('news', '/news', controller= 'ckanext.publicamundi.themes.geodata.controllers.static:Controller', action='redirect_news' )
 
         return mapper
 
