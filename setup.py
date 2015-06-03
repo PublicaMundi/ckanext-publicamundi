20488ace53b559729de86aec0f8d9a4b1646a147
Michail Alexakis <alexakis@imis.athena-innovation.gr>
2015-06-04 01:03:46 +0300

    Retab setup.py
    Conflicts:
    	setup.py

--- setup.py
+++ setup.py
@@ -1,31 +1,31 @@
 from setuptools import setup, find_packages
 import sys, os
 
-version = '1.2dev'
+version = '1.3dev'
 
 setup(
-	name='ckanext-publicamundi',
-	version=version,
-	description="A collection of CKAN plugins developed for PublicaMundi project",
-	long_description="""\
-	""",
-	classifiers=[], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
-	keywords='',
-	author='Michail Alexakis',
-	author_email='alexakis@imis.athena-innovation.gr',
-	url='https://github.com/PublicaMundi/ckanext-publicamundi',
-	license='',
-	packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
-	namespace_packages=['ckanext', 'ckanext.publicamundi'],
-	include_package_data=True,
-	paster_plugins=['pylons', 'ckan'],
-	zip_safe=False,
+    name='ckanext-publicamundi',
+    version=version,
+    description="A collection of CKAN plugins developed for PublicaMundi project",
+    long_description="""\
+    """,
+    classifiers=[], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+    keywords='ckan catalog open-data',
+    author='Michail Alexakis',
+    author_email='alexakis@imis.athena-innovation.gr',
+    url='https://github.com/PublicaMundi/ckanext-publicamundi',
+    license='GPLv3',
+    packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+    namespace_packages=['ckanext', 'ckanext.publicamundi'],
+    include_package_data=True,
+    paster_plugins=['pylons', 'ckan'],
+    zip_safe=False,
     install_requires=[
-		# -*- Extra requirements: -*-
+        # -*- Extra requirements: -*-
         # Note: Moved under requirements.txt
-	],
-	entry_points=\
-	"""
+    ],
+    entry_points=\
+    """
         [ckan.plugins]
 
         publicamundi_dataset = ckanext.publicamundi.plugins:DatasetForm
@@ -40,7 +40,9 @@ setup(
 
         publicamundi_vector = ckanext.publicamundi.storers.vector.plugin:VectorStorer
 
-	publicamundi_raster = ckanext.publicamundi.storers.raster.plugin:RasterStorer
+        publicamundi_raster = ckanext.publicamundi.storers.raster.plugin:RasterStorer
+       
+        publicamundi_analytics = ckanext.publicamundi.analytics.plugin:AnalyticsPlugin
 
         [paste.paster_command]
         
@@ -50,6 +52,8 @@ setup(
 
         publicamundi = ckanext.publicamundi.commands:Command
         
+        #publicamundi-example1 = ckanext.publicamundi.commands:Example1
+
         [babel.extractors]
         
         publicamundi_extract_json = ckanext.publicamundi.lib.metadata.vocabularies.babel_extractors:extract_json
@@ -59,9 +63,10 @@ setup(
         [ckan.celery_task]
         
         vector_tasks = ckanext.publicamundi.storers.vector.celery_import:task_imports
-	
-	raster_taks = ckanext.publicamundi.storers.raster.celery_import:task_imports
-        """,
+    
+        raster_taks = ckanext.publicamundi.storers.raster.celery_import:task_imports
+        
+    """,
     # The following only stands as an example. The actual message_extractors should be defined into 
     # ckan's setup.py (from where message extraction is invoked).
     message_extractors = {
