eda36d39e9ce953af2c314d2a2783693dbda48d7
Michail Alexakis <alexakis@imis.athena-innovation.gr>
2015-10-05 15:44:37 +0300

    Check corner cases while trying to determine which backend was used (vectorstorer)

--- ckanext/publicamundi/storers/vector/lib/utils.py
+++ ckanext/publicamundi/storers/vector/lib/utils.py
@@ -3,14 +3,15 @@ from pylons import config
 PUBLISHED_AT_GEOSERVER = "geoserver"
 PUBLISHED_AT_MAPSERVER = "mapserver"
 
-def get_publishing_server(resource, mapservers_context = None):
+def get_publishing_server(resource, backend_context=None):
     ''' Compares a WMS resource URL to mapping server
     urls found in config file. Returns mapserver or
     geoserver.'''
 
     geoserver_url = None
     mapserver_url = None
-    if not mapservers_context:
+    
+    if not backend_context:
         # used by ckan instance
         try:
             geoserver_url = config['ckanext.publicamundi.vectorstorer.geoserver.url']
@@ -24,22 +25,25 @@ def get_publishing_server(resource, mapservers_context = None):
     else:
         # used by celery tasks
         try:
-            geoserver_context = mapservers_context['geoserver_context']
-            geoserver_url = geoserver_context['url']
+            geoserver_context = backend_context['geoserver_context']
+            geoserver_url = geoserver_context['url'] if geoserver_context else None
         except KeyError:
             pass
         try:
-            mapserver_context = mapservers_context['mapserver_context']
-            mapserver_url = mapserver_context['url']
+            mapserver_context = backend_context['mapserver_context']
+            mapserver_url = mapserver_context['url'] if mapserver_context else None
         except KeyError:
             pass
+    
     if resource['format'] == 'wms':
-        if geoserver_url in resource['wms_server']:
+        if geoserver_url and (geoserver_url in resource['wms_server']):
             return PUBLISHED_AT_GEOSERVER
-        elif mapserver_url in resource['wms_server']:
+        elif mapserver_url and (mapserver_url in resource['wms_server']):
             return PUBLISHED_AT_MAPSERVER
     elif resource['format'] == 'wfs':
-        if geoserver_url in resource['wfs_server']:
+        if geoserver_url and (geoserver_url in resource['wfs_server']):
             return PUBLISHED_AT_GEOSERVER
-        elif mapserver_url in resource['wfs_server']:
+        elif mapserver_url and (mapserver_url in resource['wfs_server']):
             return PUBLISHED_AT_MAPSERVER
+
+    return None
