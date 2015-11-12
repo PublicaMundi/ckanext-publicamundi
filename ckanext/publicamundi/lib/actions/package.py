19e856f3ba01d41922d3a63c7f09c9c264ce12da
Stelios <smanousopoulos@gmail.com>
2015-11-12 15:08:58 +0200

    minor fix

--- ckanext/publicamundi/lib/actions/package.py
+++ ckanext/publicamundi/lib/actions/package.py
@@ -82,12 +82,16 @@ def dcat_export(context, data_dict):
         tmp_xml = xser.dumps()
         # Transform using XSLT
         from lxml import etree
+
         tmp_dom = etree.fromstring(tmp_xml)
-        xsl_filename = 'iso-19139-to-dcat-ap.xsl'
-        dcat_xslt = etree.parse(xsl_filename)
-        dcat_transform = etree.XSLT(dcat_xslt)
-        tmp_newdom = dcat_transform(tmp_dom)
-        result = etree.tostring(tmp_newdom, pretty_print=True))
+
+        xsl_file = reference_data.get_path('xsl/isotc211.org-2005/iso-19139-to-dcat-ap.xsl')
+        with open(xsl_file, 'r') as fp:
+            dcat_xslt = etree.parse(fp)
+
+            dcat_transform = etree.XSLT(dcat_xslt)
+            tmp_newdom = dcat_transform(tmp_dom)
+            result = etree.tostring(tmp_newdom, pretty_print=True)
 
     return result
 
