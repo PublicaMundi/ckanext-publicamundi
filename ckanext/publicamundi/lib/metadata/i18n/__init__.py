21969016537c6df900b0ac6b1728f5aadc8bade3
Michail Alexakis <alexakis@imis.athena-innovation.gr>
2015-11-15 12:31:18 +0200

    Create Python package to deal with multilingual metadata objects (placeholder)

---
+++ ckanext/publicamundi/lib/metadata/i18n/__init__.py
@@ -0,0 +1,7 @@
+'''Multilinguality for metadata objects
+'''
+
+from ckanext.publicamundi.lib import languages
+
+language_codes = languages.get_all('iso-639-1').keys()
+
---
+++ ckanext/publicamundi/lib/metadata/i18n/ibase.py
@@ -0,0 +1,7 @@
+import zope.interface
+import zope.schema
+
+from ckanext.publicamundi.lib import vocabularies
+from ckanext.publicamundi.lib.metadata.types import IMetadata
+
+# Todo Define interfaces
